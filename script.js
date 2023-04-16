4c05f669b4222201a0039a5fa16b97300b51f4de
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-04-16 15:43:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	var fname =document.getElementById('fname').value;
+	console.log(fname);
 
+	var lname = document.getElementById("lname").value;
+	
+	console.log(fname,lname);
+alert(fname+" "+lname);
 }
